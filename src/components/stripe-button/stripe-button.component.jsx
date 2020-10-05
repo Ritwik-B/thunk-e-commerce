5cab2413b90eae6e77e050e9c0606f396e6786e4
Ritwik Bhanot <rbhanot999@gmail.com>
2020-10-05 17:48:32 +0530

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51HYs3YG6AC6rBwqT7LdkwVQPfSG7asqs42tb6xP6vtQJu8Ln1f05ji61MoEYkKIrqbeb8CB6Iehmk7CEfHc9dqfL0011J81K1g";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
